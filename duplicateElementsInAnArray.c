2f3ad89bbd24931708321c994d6a76bfe180cfa0
SURINDER SINGH <147221545+SINGHSURINDER1@users.noreply.github.com>
2023-12-11 07:18:59 +0530

    Add files via upload

---
+++ duplicateElementsInAnArray.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    int arr[size];
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Duplicate elements in the array are: ");
+    for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (arr[i] == arr[j]) {
+                printf("%d ", arr[i]);
+                break; 
+            }
+        }
+    }
+
+    return 0;
+}
